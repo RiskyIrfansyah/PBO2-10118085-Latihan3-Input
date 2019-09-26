9b2e1e4e8a59b7bceb048c734ed0e09b7c9af9f1
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-09-26 08:02:01 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118085/latihan3/PBO210118085Latihan3.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan3;
+
+import java.util.Scanner;
+
+/**
+ * @author 
+ * NAMA                 : RISKY IRFRANSYAH
+ * KELAS                : IF-2
+ * NIM                  : 10118085
+ * DESKRIPSI PROGRAM    : Memasukkan nilai dari keyboard
+ * @author RISKY
+ */
+public class PBO210118085Latihan3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+        
+        
+    }
+    
+}
